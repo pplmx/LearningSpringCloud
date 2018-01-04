387b861624dcca7b4c98afe740a19740b810ac2a
mystic_cg <215104920@qq.com>
2018-01-04 15:51:24 +0800

    add Hystrix breaker

--- Ribbon/src/main/java/individual/cy/cloud/service/HelloService.java
+++ Ribbon/src/main/java/individual/cy/cloud/service/HelloService.java
@@ -1,5 +1,6 @@
 package individual.cy.cloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -21,8 +22,13 @@ public class HelloService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name=" + name, String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi," + name + ",sorry,error!";
     }
 
 }
